835a90cad96642195e19b95376690cd71de8252b
testingforforwhat <2273664Z@student.gla.ac.uk>
2023-04-20 05:53:05 +0100

    test

--- test.cpp
+++ test.cpp
@@ -8,3 +8,12 @@ void test::test_start()
     player->play();
     qDebug()<<"test";
 }
+
+void test::stopaudio()
+{
+    QMediaPlayer *player = new QMediaPlayer;
+//    player->setMedia(QUrl::fromLocalFile("C:\\Users\\23194\\Downloads\\test_.mp3"));
+//    player->setVolume(50);
+    player->stop();
+    qDebug()<<"test";
+}
