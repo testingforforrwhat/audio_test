aa36bd01e53690789c35c1668bfcc33564f6c6b9
testingforforwhat <2273664Z@student.gla.ac.uk>
2023-04-20 05:09:21 +0100

    test

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include "test.h"
+
+void test::test_start()
+{
+    QMediaPlayer *player = new QMediaPlayer;
+    player->setMedia(QUrl::fromLocalFile("C:\\Users\\23194\\Downloads\\test_.mp3"));
+    player->setVolume(50);
+    player->play();
+    qDebug()<<"test";
+}
---
+++ test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+#include <QMediaPlayer>
+#include <QDebug>
+
+
+
+class test
+{
+public:
+    void test_start();
+    void stopaudio();
+};
+
+#endif // TEST_H
